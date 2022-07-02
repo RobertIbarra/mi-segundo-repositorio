c77236ea442329885194be2faac3373222a89903
RobertIbarra <robemace441@gmail.com>
2022-07-02 16:49:21 -0500

    subiendo archivo

---
+++ eje.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+     <html>
+     <head>
+       <title>Hipervínculo a otra página del mismo sitio 3</title>
+       <meta charset="UTF-8">
+     </head>
+     <body bgcolor="#FFEFBA">
+         <center>
+        <h1>PRODUCTOS DE LINEA BLANCA</h1>
+    </center>
+        <center>
+            <img src="./IMG/CASA.jpeg" width="500" height="300" alt="500"></a>
+       </center>
+       <center>
+       </table><br>
+    <form action="Agora.php" method="post">
+        <font color="white" size="5">
+        <table bgcolor="#595959" align="center">
+            <tr>
+                <td>
+                    <label for="">Usuario:</label>
+                </td>
+                <td>
+                    <input type="text" name="usua" id="">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="">Producto: </label>
+                </td>
+                <td>
+                    <select name="categoria" id="">
+                        <option selected disabled>Escoger producto</option>
+                        <option value="a">Lavadora</option>
+                        <option value="b">Televisor</option>
+                        <option value="c">Congelador</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="">Cuotas: </label>
+                </td>
+                <td>
+                    <select name="categoria" id="">
+                    <option selected disabled>Escoger producto</option>
+                    <option value="a">3</option>
+                    <option value="b">6</option>
+                    <option value="c">9</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input type="submit" value="Cotizar">
+                </td>
+            </tr>
+        </font>
+    </table>
+    </form>
+    </center>
+     </body>
+     </html>
